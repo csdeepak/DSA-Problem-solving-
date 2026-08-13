b37b9711ad2658b24da6a972e78e093f1f80a1a1
C S DEEPAK <146485921+csdeepak@users.noreply.github.com>
2026-08-14 01:54:47 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/1184. Car Pooling/Solution.py
+++ LeetCode/Python/Medium/1184. Car Pooling/Solution.py
@@ -1,7 +1,14 @@
 class Solution(object):
     def carPooling(self, trips, capacity):
-        for i in range(len(trips)-1):
-            if trips[i][0]+trips[i+1][0]>capacity and trips[i][2]<trips[i+1][2]:
+        diff=[0]*1001
+        for passengers,start,end in trips:
+            diff[start]+=passengers
+            diff[end]-=passengers
+
+        current=0
+        for i in range(1001):
+            current+=diff[i]
+            if current>capacity: 
                 return False
-                break
-        return True       
+        return True        
+        
