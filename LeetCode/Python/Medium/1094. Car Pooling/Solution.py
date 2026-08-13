57d7b80dafb1099cce9ec9ef132bbf4085f8c31d
C S DEEPAK <146485921+csdeepak@users.noreply.github.com>
2026-08-14 01:44:12 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/1094. Car Pooling/Solution.py
+++ LeetCode/Python/Medium/1094. Car Pooling/Solution.py
@@ -3,5 +3,5 @@ class Solution(object):
         for i in range(len(trips)-1):
             if trips[i][0]+trips[i+1][0]>capacity and trips[i][2]<trips[i+1][2]:
                 return False
-            else : 
-                return True    
+                break
+        return True       
