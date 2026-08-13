ffd939d39b5f8d2adfbe05a6bd5ce4a66f873e01
C S DEEPAK <146485921+csdeepak@users.noreply.github.com>
2026-08-14 02:36:06 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/724. Find Pivot Index/Solution.py
+++ LeetCode/Python/Easy/724. Find Pivot Index/Solution.py
@@ -4,18 +4,14 @@ class Solution(object):
         for i in range(len(nums)):   
             current+=nums[i]
             nums[i]=current
-        n=0   
+           
         for i in range(0,len(nums)):
             left = 0 if i == 0 else nums[i - 1]
             if left==nums[len(nums)-1]-nums[i]:
-                n=i
-                break
-            else :
-                n=i
-        if n==(len(nums)-1):
-            return -1
-        else:
-            return n
+                i
+                return i
+
+        return -1
                
 
         
