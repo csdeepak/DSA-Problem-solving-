b5b9155af6db53efbf21c0e2cd6c1699ea2abbde
C S DEEPAK <146485921+csdeepak@users.noreply.github.com>
2026-08-14 01:51:47 +0530

    Add Python Solution to Patterns/Heap & Priority Queue

--- LeetCode/Patterns/Heap & Priority Queue/1094. Car Pooling/Solution.py
+++ LeetCode/Patterns/Heap & Priority Queue/1094. Car Pooling/Solution.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def carPooling(self, trips, capacity):
-        for i in range(len(trips)-1):
-            if trips[i][0]+trips[i+1][0]>capacity and trips[i][2]<trips[i+1][2]:
-                return False
-                break
-        return True       
+        diff=[0]*1001
+        for passengers,
+        
