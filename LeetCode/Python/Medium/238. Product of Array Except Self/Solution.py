ac3583afb2fddcb4305b55ea49509b17eb43c7b7
C S DEEPAK <146485921+csdeepak@users.noreply.github.com>
2026-08-14 00:03:04 +0530

    Add Medium Python Solution from LeetCode

--- LeetCode/Python/Medium/238. Product of Array Except Self/Solution.py
+++ LeetCode/Python/Medium/238. Product of Array Except Self/Solution.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def productExceptSelf(self, nums):
-        n = len(nums)
-        result = [1] * n
-
-        # Prefix products
-        prefix = 1
+        n=len(nums)
+        result=[1]*n
+        
+        prefix=1
         for i in range(n):
-            result[i] = prefix
-            prefix *= nums[i]
-
-        # Suffix products
-        suffix = 1
-        for i in range(n - 1, -1, -1):
-            result[i] *= suffix
-            suffix *= nums[i]
+            result[i]=prefix
+            prefix*=nums[i]
+            
+        sufix=1
+        for i in range(n-1,-1,-1):
+            result[i]*=sufix
+            sufix*=nums[i]
 
         return result
