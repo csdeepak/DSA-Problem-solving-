526adee0f88e7e917433294ddaa52afd8401e355
C S DEEPAK <146485921+csdeepak@users.noreply.github.com>
2026-08-15 12:06:32 +0530

    Add Medium Python Solution from LeetCode

---
+++ LeetCode/Python/Medium/53. Maximum Subarray/Solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        sum1=0
+        max1=0
+        for x in nums:
+            sum1+=x
+            if sum1<0:
+                sum1=0
+            max1=max(max1,sum1)
+        return max1
+            
